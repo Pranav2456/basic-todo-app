8c0d5e7546eeb123af641403d61aee1cac4609de
Pranav2456 <pranavvinodan@gmail.com>
2024-01-03 21:35:11 +0530

    Some Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { CreateTodo } from "./components/CreateTodo";
 import { Todos } from "./components/Todos";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
-  
+  const fetchTodos = async () => {
+    const res = await fetch ("http://localhost:3000/todos");
+    const data = await res.json();
+    setTodos(data.todos);
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, [])
 
   return (
     <div>
