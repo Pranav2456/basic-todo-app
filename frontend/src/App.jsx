6c686d6d05256b2a4e452aa15327586ce0b71872
Pranav2456 <pranavvinodan@gmail.com>
2024-01-06 21:00:18 +0530

    Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,18 @@ import { Todos } from "./components/Todos";
 function App() {
   const [todos, setTodos] = useState([]);
 
-  const fetchTodos = async () => {
+  /*const fetchTodos = async () => {
     const res = await fetch ("http://localhost:3000/todos");
     const data = await res.json();
     setTodos(data.todos);
-  }
+  } */
 
   useEffect(() => {
-    fetchTodos();
+    fetch("http://localhost:3000/todos")
+    .then(async (res) => {
+      const data = await res.json();
+      setTodos(data.todos);
+    })
   }, [])
 
   return (
